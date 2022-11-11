a6f6e113e929bafbc4a300ca35a2167762b4f350
Tashi Sherpa <68435081+TSherpa10@users.noreply.github.com>
2022-11-10 19:01:41 -0500

    Initialized react app, landing page

---
+++ src/Components/Landing.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from "react-dom/client";
+import './Landing.css';
+
+function Landing() {
+    return (
+        <div className='fullLanding'>
+            <h1>Trevity</h1>
+        </div>
+    )
+}
+
+export default Landing
